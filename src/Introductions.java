b74d85f64d83e89b59ecc56ca6e09645f2214aeb
Quertte <a.injen90@gmail.com>
2020-07-09 23:13:07 +0300

    My first commit

---
+++ src/Introductions.java
@@ -0,0 +1,13 @@
+public class Introductions {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i + 1 + ")Hello! My name is Andrew" + "   ");
+        }
+
+        System.out.println('\n');
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + 1 + ")I want to be a java developer!");
+        }
+    }
+}
