b7123965f296c317f950b69e65c6efbf221d3f47
Quertte <a.injen90@gmail.com>
2020-08-06 13:42:50 +0300

    Lesson9 HomeWork Update1

--- src/main/java/module/Lesson9.java
+++ src/main/java/module/Lesson9.java
@@ -7,10 +7,10 @@ public class Lesson9 {
         //1 задание
         String st = "Java"; // В пуле
         String st2 = new String("Java"); // не в пуле
-        String st3 = st2.intern(); // не в пуле, ссылкается
+        String st3 = st2.intern(); // в пуле
         String st4 = "java"; //В пуле
         String st5 = null; // В задании опечатка - там просто st (была бы ошибка компиляции) // не в пуле
-        String st6 = new String("JAVA"); //не в пуле
+        String st6 = new String("JAVA"); // одновременно и в пуле и нет
 
         // 2 задание
         String string = "London - is a capital of England";
@@ -19,16 +19,18 @@ public class Lesson9 {
 
         //3 задание
         Scanner scanner1 = new Scanner(System.in);
-        subStr("Andrew", "result");
+        System.out.println("Введите 2 слова с четным количеством букв: ");
+        subStr(scanner1.nextLine(), scanner1.nextLine());
 
         //Сканер для 4ого задания
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите слово которое хотите развернуть: ");
         reverse(scanner.nextLine());
     }
 
     //Метод для 3 задания
     private static void subStr(String x, String y) {
-        System.out.println(x.substring(0, 3) + y.substring(3, y.length()));
+        System.out.println(x.substring(0, x.length() / 2) + y.substring(y.length() / 2, y.length()));
     }
 
     //4 Задание
