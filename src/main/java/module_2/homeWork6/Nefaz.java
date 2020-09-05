cdf7716791faad38213b3e851f82ca2b204d4b19
Quertte <a.injen90@gmail.com>
2020-09-05 10:33:15 +0300

    Изменен метод leave()

--- src/main/java/module_2/homeWork6/Nefaz.java
+++ src/main/java/module_2/homeWork6/Nefaz.java
@@ -1,7 +1,7 @@
 package module_2.homeWork6;
 
 public class Nefaz extends Bus{
-    public Nefaz(int size, int power, int numberSeats) {
+    public Nefaz(int size, int power, int numberSeats)  {
         super(size, power, numberSeats);
     }
 }
